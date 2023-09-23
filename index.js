bd0ca2b05d00a0c6d8ca9e9cd48a10fac1c40c46
Dev Rastogi <118082711+Devrastogii@users.noreply.github.com>
2023-09-23 14:00:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './index.css';
+
+import App from './App';
+import Login from './components/loginsignup/Login';
+import Register from './components/loginsignup/Register';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+
+ 
+
+    <BrowserRouter>
+      <Routes>
+
+        <Route exact path='/' element={<Login />}></Route>
+        <Route exact path='/register' element={<Register />}></Route>
+        <Route exact path='/user' element={<App />}>         
+        </Route>
+
+      </Routes>
+    </BrowserRouter>
+
+
+    
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
